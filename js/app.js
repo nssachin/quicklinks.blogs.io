c8caaf25be397d9f4cc432f809de55bf184631e1
SachinNS <sachinjois@gmail.com>
2021-10-25 20:05:21 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var jqueryNoConflict = jQuery;
-var source = 'https://gist.githubusercontent.com/nssachin/5d341e7e4c9fa4fad097f53e7f10470b/raw/cfc4a7506c870b640ffdc78598c059de86d9c0a9/';
+var source = 'https://gist.githubusercontent.com/nssachin/5d341e7e4c9fa4fad097f53e7f10470b/raw/fd081a72d80825860130c8244c973be34e9f87f3/';
 
 //begin with the main function
 jqueryNoConflict(document).ready(function() {
